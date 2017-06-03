052c71bab4c809d6b48683f5d8c51bfc9e8f7bf7
nishantranjansingh <nrsenc@gmail.com>
2017-06-03 14:59:55 -0400

    first local commit

---
+++ app/src/main/java/com/example/nishantranjansingh/myapplication/ScanActivity.java
@@ -0,0 +1,73 @@
+package com.example.nishantranjansingh.myapplication;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.content.ComponentCallbacks2;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
+/**
+ * Created by jhansi on 28/03/15.
+ */
+public class ScanActivity extends Activity implements IScanner, ComponentCallbacks2 {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
+        setContentView(R.layout.scan_layout);
+        init();
+    }
+
+    private void init() {
+        PickImageFragment fragment = new PickImageFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ScanConstants.OPEN_INTENT_PREFERENCE, getPreferenceContent());
+        fragment.setArguments(bundle);
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content, fragment);
+        fragmentTransaction.commit();
+    }
+
+    protected int getPreferenceContent() {
+        return getIntent().getIntExtra(ScanConstants.OPEN_INTENT_PREFERENCE, 0);
+    }
+
+    @Override
+    public void onBitmapSelect(Uri uri) {
+        ScanFragment fragment = new ScanFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(ScanConstants.SELECTED_BITMAP, uri);
+        fragment.setArguments(bundle);
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content, fragment);
+        fragmentTransaction.addToBackStack(ScanFragment.class.toString());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onScanFinish(Uri uri) {
+        ResultFragment fragment = new ResultFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(ScanConstants.SCANNED_RESULT, uri);
+        fragment.setArguments(bundle);
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content, fragment);
+        fragmentTransaction.addToBackStack(ResultFragment.class.toString());
+        fragmentTransaction.commit();
+    }
+
+}
---
+++ app/src/main/java/com/example/nishantranjansingh/myapplication/ScanFragment.java
@@ -0,0 +1,392 @@
+package com.example.nishantranjansingh.myapplication;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by jhansi on 29/03/15.
+ */
+public class ScanFragment extends Fragment {
+
+    private Button scanButton;
+    private ImageView sourceImageView;
+    private FrameLayout sourceFrame;
+    private PolygonView polygonView;
+    private View view;
+    private ProgressDialogFragment progressDialogFragment;
+    private IScanner scanner;
+    private Bitmap original;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (!(activity instanceof IScanner)) {
+            throw new ClassCastException("Activity must implement IScanner");
+        }
+        this.scanner = (IScanner) activity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.scan_fragment_layout, null);
+        init();
+        return view;
+    }
+
+    public ScanFragment() {
+
+    }
+
+    private void init() {
+        sourceImageView = (ImageView) view.findViewById(R.id.sourceImageView);
+        scanButton = (Button) view.findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(new ScanButtonClickListener());
+        sourceFrame = (FrameLayout) view.findViewById(R.id.sourceFrame);
+        polygonView = (PolygonView) view.findViewById(R.id.polygonView);
+        sourceFrame.post(new Runnable() {
+            @Override
+            public void run() {
+                original = getBitmap();
+                if (original != null) {
+                    setBitmap(original);
+                }
+            }
+        });
+    }
+
+    private Bitmap getBitmap() {
+        Uri uri = getUri();
+        try {
+            Bitmap bitmap = Utils.getBitmap(getActivity(), uri);
+            getActivity().getContentResolver().delete(uri, null, null);
+            return bitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Uri getUri() {
+        Uri uri = getArguments().getParcelable(ScanConstants.SELECTED_BITMAP);
+        return uri;
+    }
+
+    private void setBitmap(Bitmap original) {
+        Bitmap scaledBitmap = scaledBitmap(original, sourceFrame.getWidth(), sourceFrame.getHeight());
+        sourceImageView.setImageBitmap(scaledBitmap);
+        Bitmap tempBitmap = ((BitmapDrawable) sourceImageView.getDrawable()).getBitmap();
+        Map<Integer, PointF> pointFs = getEdgePoints(tempBitmap);
+        polygonView.setPoints(pointFs);
+        polygonView.setVisibility(View.VISIBLE);
+        int padding = (int) getResources().getDimension(R.dimen.scanPadding);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(tempBitmap.getWidth() + 2 * padding, tempBitmap.getHeight() + 2 * padding);
+        layoutParams.gravity = Gravity.CENTER;
+        polygonView.setLayoutParams(layoutParams);
+    }
+
+    private Map<Integer, PointF> getEdgePoints(Bitmap tempBitmap) {
+        List<PointF> pointFs = getContourEdgePoints(tempBitmap);
+        Map<Integer, PointF> orderedPoints = orderedValidEdgePoints(tempBitmap, pointFs);
+        return orderedPoints;
+    }
+
+    private List<PointF> getContourEdgePoints(Bitmap tempBitmap) {
+        List<PointF> pointFs = new ArrayList<>();
+        try {
+            Mat original_image = new Mat();
+            org.opencv.android.Utils.bitmapToMat(tempBitmap, original_image);
+            Point[] points = findRectangle(original_image);
+            float x1 = (float)points[0].x;
+            float x2 = (float)points[1].x;
+            float x3 = (float)points[2].x;
+            float x4 = (float)points[3].x;
+
+            float y1 = (float)points[0].y;
+            float y2 = (float)points[1].y;
+            float y3 = (float)points[2].y;
+            float y4 = (float)points[3].y;
+            pointFs.add(new PointF(x1, y1));
+            pointFs.add(new PointF(x2, y2));
+            pointFs.add(new PointF(x3, y3));
+            pointFs.add(new PointF(x4, y4));
+        }catch (Exception ae){
+
+        }
+
+        return pointFs;
+    }
+
+    private Point[] findRectangle(Mat src) throws Exception {
+        Mat blurred = src.clone();
+        Imgproc.medianBlur(src, blurred, 9);
+
+        Mat gray0 = new Mat(blurred.size(), CvType.CV_8U), gray = new Mat();
+
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+        List<Mat> blurredChannel = new ArrayList<Mat>();
+        blurredChannel.add(blurred);
+        List<Mat> gray0Channel = new ArrayList<Mat>();
+        gray0Channel.add(gray0);
+
+        MatOfPoint2f approxCurve;
+
+        double maxArea = 0;
+        int maxId = -1;
+        List<MatOfPoint2f> squares = new ArrayList<>();
+        for (int c = 0; c < 3; c++) {
+            int ch[] = { c, 0 };
+            Core.mixChannels(blurredChannel, gray0Channel, new MatOfInt(ch));
+
+            int thresholdLevel = 1;
+            for (int t = 0; t < thresholdLevel; t++) {
+                if (t == 0) {
+                    Imgproc.Canny(gray0, gray, 10, 20, 3, true); // true ?
+                    Imgproc.dilate(gray, gray, new Mat(), new Point(-1, -1), 1); // 1
+                    // ?
+                } else {
+                    Imgproc.adaptiveThreshold(gray0, gray, thresholdLevel,
+                            Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,
+                            Imgproc.THRESH_BINARY,
+                            (src.width() + src.height()) / 200, t);
+                }
+
+                Imgproc.findContours(gray, contours, new Mat(),
+                        Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                for (MatOfPoint contour : contours) {
+                    MatOfPoint2f temp = new MatOfPoint2f(contour.toArray());
+
+                    double area = Imgproc.contourArea(contour);
+                    approxCurve = new MatOfPoint2f();
+                    Imgproc.approxPolyDP(temp, approxCurve,
+                            Imgproc.arcLength(temp, true) * 0.02, true);
+
+                    if (approxCurve.total() == 4 && area >= maxArea) {
+                        double maxCosine = 0;
+
+                        List<Point> curves = approxCurve.toList();
+                        for (int j = 2; j < 5; j++) {
+
+                            double cosine = Math.abs(angle(curves.get(j % 4),
+                                    curves.get(j - 2), curves.get(j - 1)));
+                            maxCosine = Math.max(maxCosine, cosine);
+                        }
+
+                        if (maxCosine < 0.3) {
+                            maxArea = area;
+                            maxId = contours.indexOf(contour);
+                            squares.add(approxCurve);
+                        }
+                    }
+                }
+            }
+        }
+        MatOfPoint2f temp_largest = squares.get(squares.size()-1);
+        Point[] points = temp_largest.toArray();
+        return points;
+    }
+
+    private double angle(Point p1, Point p2, Point p0) {
+        double dx1 = p1.x - p0.x;
+        double dy1 = p1.y - p0.y;
+        double dx2 = p2.x - p0.x;
+        double dy2 = p2.y - p0.y;
+        return (dx1 * dx2 + dy1 * dy2)
+                / Math.sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2)
+                + 1e-10);
+    }
+
+    private Map<Integer, PointF> getOutlinePoints(Bitmap tempBitmap) {
+        Map<Integer, PointF> outlinePoints = new HashMap<>();
+        outlinePoints.put(0, new PointF(0, 0));
+        outlinePoints.put(1, new PointF(tempBitmap.getWidth(), 0));
+        outlinePoints.put(2, new PointF(0, tempBitmap.getHeight()));
+        outlinePoints.put(3, new PointF(tempBitmap.getWidth(), tempBitmap.getHeight()));
+        return outlinePoints;
+    }
+
+    private Map<Integer, PointF> orderedValidEdgePoints(Bitmap tempBitmap, List<PointF> pointFs) {
+        Map<Integer, PointF> orderedPoints = polygonView.getOrderedPoints(pointFs);
+        if (!polygonView.isValidShape(orderedPoints)) {
+            orderedPoints = getOutlinePoints(tempBitmap);
+        }
+        return orderedPoints;
+    }
+
+    private class ScanButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Map<Integer, PointF> points = polygonView.getPoints();
+            if (isScanPointsValid(points)) {
+                new ScanAsyncTask(points).execute();
+            } else {
+                showErrorDialog();
+            }
+        }
+    }
+
+    private void showErrorDialog() {
+        SingleButtonDialogFragment fragment = new SingleButtonDialogFragment(R.string.ok, getString(R.string.cantCrop), "Error", true);
+        FragmentManager fm = getActivity().getFragmentManager();
+        fragment.show(fm, SingleButtonDialogFragment.class.toString());
+    }
+
+    private boolean isScanPointsValid(Map<Integer, PointF> points) {
+        return points.size() == 4;
+    }
+
+    private Bitmap scaledBitmap(Bitmap bitmap, int width, int height) {
+        Matrix m = new Matrix();
+        m.setRectToRect(new RectF(0, 0, bitmap.getWidth(), bitmap.getHeight()), new RectF(0, 0, width, height), Matrix.ScaleToFit.CENTER);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
+    }
+
+    private Bitmap getScannedBitmap(Bitmap original, Map<Integer, PointF> points) {
+        int width = original.getWidth();
+        int height = original.getHeight();
+        float xRatio = (float) original.getWidth() / sourceImageView.getWidth();
+        float yRatio = (float) original.getHeight() / sourceImageView.getHeight();
+
+        float x1 = (points.get(0).x) * xRatio;
+        float x2 = (points.get(1).x) * xRatio;
+        float x3 = (points.get(2).x) * xRatio;
+        float x4 = (points.get(3).x) * xRatio;
+        float y1 = (points.get(0).y) * yRatio;
+        float y2 = (points.get(1).y) * yRatio;
+        float y3 = (points.get(2).y) * yRatio;
+        float y4 = (points.get(3).y) * yRatio;
+        Log.d("", "POints(" + x1 + "," + y1 + ")(" + x2 + "," + y2 + ")(" + x3 + "," + y3 + ")(" + x4 + "," + y4 + ")");
+
+        Point topLeft = new Point(x3, y3);
+        Point topRight = new Point(x4, y4);
+        Point bottomLeft = new Point(x1, y1);
+        Point bottomRight = new Point(x2, y2);
+
+        //Bitmap _bitmap = ((ScanActivity) getActivity()).getScannedBitmap(original, x1, y1, x2, y2, x3, y3, x4, y4);
+        Bitmap _bitmap = warp(original, topLeft, topRight, bottomLeft, bottomRight);
+        return _bitmap;
+    }
+
+    Bitmap warp(Bitmap image, Point topLeft, Point topRight, Point bottomLeft, Point bottomRight) {
+        int resultWidth = (int)(topRight.x - topLeft.x);
+        int bottomWidth = (int)(bottomRight.x - bottomLeft.x);
+        if(bottomWidth > resultWidth)
+            resultWidth = bottomWidth;
+
+        int resultHeight = (int)(topLeft.y - bottomLeft.y);
+        int bottomHeight = (int)(topLeft.y - bottomRight.y);
+        if(bottomHeight > resultHeight)
+            resultHeight = bottomHeight;
+
+        Mat inputMat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC1);
+        org.opencv.android.Utils.bitmapToMat(image, inputMat);
+        Mat outputMat = new Mat(resultHeight, resultWidth, CvType.CV_8UC1);
+
+        List<Point> source = new ArrayList<>();
+        source.add(bottomLeft);
+        source.add(bottomRight);
+        source.add(topRight);
+        source.add(topLeft);
+
+
+        Mat startM = Converters.vector_Point2f_to_Mat(source);
+
+        Point ocvPOut1 = new Point(0, 0);
+        Point ocvPOut2 = new Point(resultWidth, 0);
+        Point ocvPOut3 = new Point(resultWidth, resultHeight);
+        Point ocvPOut4 = new Point(0, resultHeight);
+
+        List<Point> dest = new ArrayList<>();
+        dest.add(ocvPOut1);
+        dest.add(ocvPOut2);
+        dest.add(ocvPOut3);
+        dest.add(ocvPOut4);
+        Mat endM = Converters.vector_Point2f_to_Mat(dest);
+
+        Mat perspectiveTransform = Imgproc.getPerspectiveTransform(startM, endM);
+
+        Imgproc.warpPerspective(inputMat, outputMat, perspectiveTransform, new Size(resultWidth, resultHeight));
+
+        Bitmap output = Bitmap.createBitmap(resultWidth, resultHeight, Bitmap.Config.ARGB_8888);
+        org.opencv.android.Utils.matToBitmap(outputMat, output);
+        return output;
+    }
+
+
+
+    private class ScanAsyncTask extends AsyncTask<Void, Void, Bitmap> {
+
+        private Map<Integer, PointF> points;
+
+        public ScanAsyncTask(Map<Integer, PointF> points) {
+            this.points = points;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showProgressDialog(getString(R.string.scanning));
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            Bitmap bitmap =  getScannedBitmap(original, points);
+            Uri uri = Utils.getUri(getActivity(), bitmap);
+            scanner.onScanFinish(uri);
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            bitmap.recycle();
+            dismissDialog();
+        }
+    }
+
+    protected void showProgressDialog(String message) {
+        progressDialogFragment = new ProgressDialogFragment(message);
+        FragmentManager fm = getFragmentManager();
+        progressDialogFragment.show(fm, ProgressDialogFragment.class.toString());
+    }
+
+    protected void dismissDialog() {
+        progressDialogFragment.dismissAllowingStateLoss();
+    }
+
+}
